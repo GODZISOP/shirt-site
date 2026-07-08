08caff6e0a0e5ec4cf85034e2f4c33ac2a51a3eb
GODZISOP <appointmentstudio@gmail.com>
2026-07-08 22:49:58 +0500

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import ShippingStats from "@/components/ShippingStats";
+import ProductCategories from "@/components/ProductCategories";
+import BrandLogos from "@/components/BrandLogos";
+import CustomerPhotos from "@/components/CustomerPhotos";
+import Testimonials from "@/components/Testimonials";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen w-full">
+    <div className="flex flex-col min-h-screen w-full bg-[#f8f9f9]">
       <Header />
       <main className="flex-1 w-full flex flex-col">
         <Hero />
         <ShippingStats />
+        <ProductCategories />
+        <BrandLogos />
+        <CustomerPhotos />
+        <Testimonials />
       </main>
+      <Footer />
     </div>
   );
 }
